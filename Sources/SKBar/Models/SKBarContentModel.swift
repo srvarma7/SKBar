f9e5a7285ccde7285cab71ba53803a53195106bb
Sai Kallepalli <sai.kallepalli@kooapp.com>
2023-02-02 22:27:58 +0530

    Made SKBarContentModel variables public

--- Sources/SKBar/Models/SKBarContentModel.swift
+++ Sources/SKBar/Models/SKBarContentModel.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 public struct SKBarContentModel: Equatable {
-    let title: String
-    var image: UIImage? = nil
-    var selectedImage: UIImage? = nil
+    public let title: String
+    public var image: UIImage? = nil
+    public var selectedImage: UIImage? = nil
     
     public init(title: String, image: UIImage? = nil, selectedImage: UIImage? = nil) {
         self.title = title
